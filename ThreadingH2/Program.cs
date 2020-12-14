80d008650e34ea87857bb79b346a0219755b5d51
Kenneth Folke Larsen <5504355@RTS-365.dk>
2020-12-14 13:20:24 +0100

    Add project files.

---
+++ ThreadingH2/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingPractice
+{
+    class Program
+    {
+        static char ch = '*';
+        static int number = 0;
+        static object locker = new object();
+        static Thread thread;
+        static Thread thread1;
+        static AutoResetEvent autoReset = new AutoResetEvent(false);
+
+        static TimeSpan mytime = new TimeSpan(0, 0, 10);
+
+        static void Main(string[] args)
+        {
+            Program pg = new Program();
+
+            #region StartingThreads
+            //Thread thread = new Thread(new ThreadStart(pg.WorkThreadFunction));
+            //Thread thread2 = new Thread(new ThreadStart(pg.AnotherThreadFunction));
+            //thread.Start();
+            //thread2.Start();
+
+            //!Temperature Thread
+            //Thread thread2 = new Thread(new ThreadStart(pg.TemperatureThread));
+            // thread2.Start();
+            // while (thread2.IsAlive)
+            // {
+            //     Thread.Sleep(10000);
+            // }
+            // Console.WriteLine("Thread Is dead.");
+
+            //! IO Threads
+            Thread thread = new Thread(new ThreadStart(pg.InputThread));
+            Thread thread2 = new Thread(new ThreadStart(pg.OutputThread));
+            thread.Start();
+            thread2.Start();
+
+            //for (int i = 0; i < 2; i++)
+            //{
+            //    ThreadPool.QueueUserWorkItem(pg.task1);
+            //    ThreadPool.QueueUserWorkItem(pg.task2);
+            //}
+            #endregion
+            #region PoolVsObjects
+            //!ThreadPool VS objects
+            //Stopwatch stopwatch = new Stopwatch();
+            //Console.WriteLine("With threadpool");
+            //stopwatch.Start();
+            //ProcessWithThreadPoolMethod();
+            //stopwatch.Stop();
+            //Console.WriteLine("The pool took: " + stopwatch.ElapsedTicks.ToString());
+            //stopwatch.Reset();
+
+            //Console.WriteLine("Thread objects");
+            //stopwatch.Start();
+            //ProcessWithThreadMethod();
+            //stopwatch.Stop();
+            //Console.WriteLine("The objects took: " + stopwatch.ElapsedTicks.ToString());
+            //Console.ReadKey();
+            #endregion
+
+
+            //Console.ReadKey();
+        }
+
+        #region StartingThreads
+        //public void WorkThreadFunction()
+        //{
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        Console.WriteLine("C# Tråde er mega ez ");
+        //    }
+        //}
+
+        //public void AnotherThreadFunction()
+        //{
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        Console.WriteLine("yappadabbadoo");
+        //        Thread.Sleep(1000);
+        //    }
+        //}
+
+        //! Temperature Thread
+        public void TemperatureThread()
+        {
+            Random rnd = new Random();
+            int temp;
+            int alarmCount = 0;
+
+            while (alarmCount < 3)
+            {
+                temp = rnd.Next(-20, 120);
+                if (temp < 0 || temp > 100)
+                {
+                    Console.WriteLine(temp + " Temperature limit exceeded!");
+                    alarmCount++;
+                }
+                else
+                {
+                    Console.WriteLine(temp);
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        //!IO Threads
+        public void InputThread()
+        {
+            char temp;
+
+            while (true)
+            {
+                try
+                {
+                    temp = Console.ReadLine().First();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                if (char.IsLetterOrDigit(temp))
+                {
+                    ch = temp;
+                }
+            }
+        }
+
+        //!IO Threads
+        public void OutputThread()
+        {
+            while (true)
+            {
+                Console.Write(ch);
+            }
+        }
+        #endregion
+        #region PoolVsObjects
+        //public void task1(object obj)
+        //{
+        //    for (int i = 0; i <= 2; i++)
+        //    {
+        //        Console.WriteLine("Task 1 is being executed");
+        //    }
+        //}
+        //public void task2(object obj)
+        //{
+        //    for (int i = 0; i <= 2; i++)
+        //    {
+        //        Console.WriteLine("Task 2 is being executed");
+        //    }
+        //}
+
+        ////! Threadpool vs Objects
+        //static void ProcessWithThreadPoolMethod()
+        //{
+        //    for (int i = 0; i < 100000; i++)
+        //    {
+        //        ThreadPool.QueueUserWorkItem(Process);
+        //    }
+        //}
+
+        //static void ProcessWithThreadMethod()
+        //{
+        //    try
+        //    {
+        //        for (int i = 0; i < 100000; i++)
+        //        {
+        //            Thread thr = new Thread(Process);
+        //            thr.Start();
+        //        }
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine("Something went wrong");
+        //        throw;
+        //    }
+        //}
+
+        //static void Process(object callback)
+        //{
+
+        //}
+        #endregion
+
+    }
+}
